364fbe9bf72b1fed28e4f2c424441062272f6380
cjudilla15 <chris.m.judilla@gmail.com>
2024-10-13 03:17:00 +0000

    applied taybkho revisions for solcast download

--- solcast_download.py
+++ solcast_download.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
-import requests
+import requests  
 import json
 import sys
 import re
 from datetime import datetime, timedelta
+
+#def date_validation(date_text):
+#   try:
+#      datetime.strptime(date_text, '%Y-%m-%d')
+#   except ValueError:
+#      print("Incorrect Date format , it should be YYYY-MM-DD")
+#      sys.exit(1)
 
 lat = sys.argv[1]
 long = sys.argv[2]
@@ -12,28 +19,39 @@ start = sys.argv[3]
 end = sys.argv[4]
 solcasttoken = sys.argv[5]
 
-bearer = "Bearer %s" % solcasttoken
+#date_validation(start)
+#date_validation(end)
 
+bearer = f"Bearer {solcasttoken}"
 headers = {
-         'Content-Type': 'application/json',
-         'Authorization': bearer,
-     }
+   'Content-Type': 'application/json',
+   'Authorization': bearer
+}
 
-url = "https://api.solcast.com.au/data/historic/radiation_and_weather?latitude=%s&longitude=%s&period=PT5M&start=%s&end=%s&format=json&time_zone=utc&output_parameters=ghi" % (lat,long,start,end)
+url = f"https://api.solcast.com.au/data/historic/radiation_and_weather?latitude={lat}&longitude={long}&period=PT5M&start={start}&end={end}&format=json&time_zone=utc&output_parameters=ghi"
 
 response = requests.get(url, headers=headers)
 
-data = response.json()
-for i in data['estimated_actuals']:
-   period_end=i["period_end"]
-   formatted_period_end=period_end.replace('+00:00','Z')
-   datestr=formatted_period_end.replace('T',' ').replace('Z','')
-   dateobj=datetime.strptime(datestr, '%Y-%m-%d %H:%M:%S')
-    
+if response.status_code != 200:
+   print(f"Error : Received status code {response.status_code}")
+   sys.exit(1)
+
+#json_check
+
+try:
+   data = response.json()
+except json.JSONDecodeError :
+   print("Error: Failed to decode JSON response")
+   sys.exit(1)
+
+for i in data.get('estimated_actuals', []):
+   period_end=i["period_end"].replace('+00:00','Z')
+   dateobj = datetime.strptime(period_end.replace('T', ' ').replace('Z', ''), '%Y-%m-%d %H:%M:%S')
    ghi=i["ghi"]
    period=i["period"]
    duration=int(re.sub("[PTM]","",period))
-   period_start=str(dateobj - timedelta(minutes=duration))
-   formatted_period_start=period_start.replace(' ','T') + 'Z'
+   period_start=str(dateobj - timedelta(minutes=duration)).replace(' ','T') + 'Z'
+   
 
-   print (formatted_period_end,formatted_period_start,period,ghi,sep=",")
+   print (f"{period_end},{period_start},{period},{ghi}")
+
